29bf12c2fa1ce6e5d29a964bc8651667ed91e6b6
liamflannery <55472933+liamflannery@users.noreply.github.com>
2022-04-11 16:33:31 +1000

    Updating repo

--- assignments/a1/a1.py
+++ assignments/a1/a1.py
@@ -5,8 +5,12 @@ nltk.download('punkt')
 nltk.download('gutenberg')
 from sklearn.feature_extraction.text import TfidfVectorizer
 import collections
+import pandas as pd
 
-# Task 1 (2 marks)
+
+# Task 1 (1 mark)
+import collections
+nltk.download('universal_tagset')
 def count_pos(document, pos):
     """Return the number of occurrences of words with a given part of speech. To find the part of speech, use 
     NLTK's "Universal" tag set. To find the words of the document, use NLTK's sent_tokenize and word_tokenize.
@@ -15,7 +19,15 @@ def count_pos(document, pos):
     >>> count_pos('austen-sense.txt', 'VERB')
     25074
     """
-    return 0
+    word_tag = []
+    for sentance in nltk.sent_tokenize(nltk.corpus.gutenberg.raw(document)):
+        word = nltk.word_tokenize(sentance)
+        word_tag += nltk.pos_tag(word, tagset='universal')
+
+    
+    tag = [w for [n,w] in word_tag]
+    counter = collections.Counter(tag)
+    return counter[pos]
 
 # Task 2 (2 marks)
 def get_top_stem_bigrams(document, n):
@@ -27,7 +39,16 @@ def get_top_stem_bigrams(document, n):
     >>> get_top_stem_bigrams('austen-sense.txt',4)
     [(',', 'and'), ('.', "''"), (';', 'and'), (',', "''")]
     """
-    return []
+    stemmer = nltk.PorterStemmer()
+    words = []
+    bigrams = list()
+    for sentance in nltk.sent_tokenize(nltk.corpus.gutenberg.raw(document)):
+        words = [stemmer.stem(word) for word in nltk.word_tokenize(sentance)]
+        bigrams += list(nltk.bigrams(words))
+    
+    counter = collections.Counter(bigrams).most_common(n)
+    counter_small = [x for [x,y] in counter]
+    return counter_small
 
 
 # Task 3 (2 marks)
@@ -40,7 +61,19 @@ def get_same_stem(document, word):
     >>> get_same_stem('austen-sense.txt','respect')[:5]
     [('respect', 22), ('respectability', 1), ('respectable', 14), ('respectably', 1), ('respected', 3)]
     """
-    return []
+    stemmer = nltk.PorterStemmer()
+    words = dict()
+    targetStem = stemmer.stem(word)
+    for sentance in nltk.sent_tokenize(nltk.corpus.gutenberg.raw(document)):
+        for currentWord in nltk.word_tokenize(sentance):
+            if stemmer.stem(currentWord) == targetStem:
+                if(currentWord in words.keys()):
+                    words[currentWord] += 1
+                else:
+                    words[currentWord] = 1
+    listWords = words.items()
+    sortedWords = sorted(listWords)
+    return sortedWords
 
 # Task 4 (2 marks)
 def most_frequent_after_pos(document, pos):
@@ -52,7 +85,24 @@ def most_frequent_after_pos(document, pos):
     >>> most_frequent_after_pos('austen-sense.txt','NOUN')
     [(',', 5310)]
     """
-    return []
+    followWords = dict()
+    prevWord = False;
+    for sentance in nltk.sent_tokenize(nltk.corpus.gutenberg.raw(document)):
+         currentWord = nltk.word_tokenize(sentance)
+         for tokenWord in nltk.pos_tag(currentWord, tagset='universal'):
+             currentPart = tokenWord[1]
+             
+             if(prevWord):
+                 if(tokenWord[0] in followWords.keys()):
+                    followWords[tokenWord[0]] += 1
+                 else:
+                    followWords[tokenWord[0]] = 1
+             prevWord = False;
+             if(currentPart == pos):
+                 prevWord = True;         
+              
+    followWords = sorted(followWords.items(), key = lambda item: item[1], reverse = True)
+    return [followWords[0]]
 
 # Task 5 (2 marks)
 def get_word_tfidf(text):
@@ -66,7 +116,20 @@ def get_word_tfidf(text):
     >>> get_word_tfidf('Brutus is a honourable person')
     [('brutus', 0.8405129362379974), ('honourable', 0.4310718596448824), ('person', 0.32819971943754456)]
     """
-    return []
+    tfidfVectorizer = TfidfVectorizer(input='content',stop_words='english')
+    data = [nltk.corpus.gutenberg.raw(f) for f in nltk.corpus.gutenberg.fileids()]
+    tfidf = tfidfVectorizer.fit_transform(data)
+    df = pd.DataFrame(tfidf[0].T.todense(), index=tfidfVectorizer.get_feature_names(), columns=["TF-IDF"])
+    df = df.sort_values('TF-IDF', ascending=False)
+    result = []
+    df = df["TF-IDF"].to_dict()
+    dfItems = df.items()
+   
+    for word in text.split(" "):
+         for i in dfItems:
+            if(i[0] == word.lower() and i[1] > 0):
+                result += i
+    return result
 
 
 # DO NOT MODIFY THE CODE BELOW
